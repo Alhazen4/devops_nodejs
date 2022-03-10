a6ff3d564db8cba326ce7571c7657433614903ce
Alhazen4 <62123569+Cybernonk404@users.noreply.github.com>
2022-03-10 10:52:47 +0700

    add3

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require('http');
 const fs = require('fs')
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/html"});
     fs.readFile('index.html', function(error, data) {
+        response.writeHead(200, {"Content-Type": "text/html"});
         if (error) {
             response.writeHead(404)
             response.write('Error')
