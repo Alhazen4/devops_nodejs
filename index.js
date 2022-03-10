b0b9b20c672de9f6c3c3b44b10c704aa02b384f3
Alhazen4 <62123569+Cybernonk404@users.noreply.github.com>
2022-03-10 15:32:48 +0700

    '

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express');
+const http = require('http');
+const fs = require('fs')
 
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("<h2> ALHAMDULILLAH </h2>");
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile('index.html', function(error, data) {
+        if (error) {
+            response.writeHead(404)
+            response.write('Error')
+        } else {
+            response.write(data)
+        }
+    })
+    response.end();
 });
 
 const port = process.env.PORT || 1337;
-app.listen(port);
+server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
