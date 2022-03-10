e90921ef92d964868e86b898b474e4cd8c1db396
Alhazen4 <62123569+Cybernonk404@users.noreply.github.com>
2022-03-10 15:35:36 +0700

    ;

--- index.js
+++ index.js
@@ -3,15 +3,7 @@ const fs = require('fs')
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile('index.html', function(error, data) {
-        if (error) {
-            response.writeHead(404)
-            response.write('Error')
-        } else {
-            response.write(data)
-        }
-    })
-    response.end();
+    response.end("Alhamdulillah");
 });
 
 const port = process.env.PORT || 1337;
