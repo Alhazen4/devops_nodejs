65f306027e284f24247110889acff249c784c491
Alhazen4 <62123569+Cybernonk404@users.noreply.github.com>
2022-03-10 16:10:41 +0700

    /

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 
-const path = __dirname + '/views';
+const path = __dirname + '/views/';
 
-app.get("/", (req, res) => {
-    res.send("<h2> ALHAMDULILLAH </h2>");
-});
+// app.get("/", (req, res) => {
+//     res.send("<h2> ALHAMDULILLAH </h2>");
+// });
 
 router.use(function (req,res,next) {
     console.log('/' + req.method);
     next();
-  });
+});
   
-  router.get('/', function(req,res){
+router.get('/', function(req,res){
     res.sendFile(path + 'index.html');
-  });
-  
+});
 
+app.use(express.static(path));
+app.use('/', router);
+  
 const port = process.env.PORT || 1337;
 
 app.listen(port, () => console.log('Listening on port ${port}'))
